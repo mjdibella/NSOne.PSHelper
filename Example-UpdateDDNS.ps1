c7464b445f1dd456bd5d86a58f50d6d5df02f91f
mjdibella <74016254+mjdibella@users.noreply.github.com>
2022-02-05 10:57:34 -0800

    Add files via upload

--- Example-UpdateDDNS.ps1
+++ Example-UpdateDDNS.ps1
@@ -1,3 +1,9 @@
 import-module NSOne.PSHelper.psm1
 Connect-NSOne -apitoken <your-ns1-apikey>
-Get-NSOneRecord <zone> <domain> A | Set-NSOneRecordAnswer -index 0 $(Get-NSOneDynamicIP -uri <checkuri>) | Update-NSOneRecord
+$oldIP = Get-NSOneRecord <zone> <domain> A | Get-NSOneRecordAnswer -index 0
+$newIP = Get-NSOneDynamicIP -uri <checkuri>
+if ($newIP) {
+    if ($oldIP -ne $newIP) {
+        Get-NSOneRecord <zone> <domain> A | Set-NSOneRecordAnswer -index 0 $newIP | Update-NSOneRecord
+    }
+}
