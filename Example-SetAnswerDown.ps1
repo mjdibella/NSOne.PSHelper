b2d8c2a5f79568048a51238fea2291dab43286fa
mjdibella <74016254+mjdibella@users.noreply.github.com>
2022-02-05 14:41:27 -0800

    Update Example-SetAnswerDown.ps1

--- Example-SetAnswerDown.ps1
+++ Example-SetAnswerDown.ps1
@@ -1,3 +1,3 @@
 import-module NSOne.PSHelper.psm1
 Connect-NSOne -apitoken <your-ns1-apikey>
-Get-NSOneRecord <zone> <domain> <TYPE> | Set-NSOneRecordAnswerMetaProperty <answer> Up True | Update-NSOneRecord
+Get-NSOneRecord <zone> <domain> <TYPE> | Set-NSOneRecordAnswerMetaProperty <answer> Up False | Update-NSOneRecord
